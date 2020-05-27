5e1993e5821f1bf5fc778086d7ddbe9fbd52852f
Felipe Villela <fdbva@users.noreply.github.com>
2020-05-27 07:13:48 -0300

    [Step06] Mvc pegando JWT do WebApi e usando no Header para fazer request Http (#2)

--- 20GRPED.MVC2.Mvc/HttpServices/LivroHttpService.cs
+++ 20GRPED.MVC2.Mvc/HttpServices/LivroHttpService.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using _20GRPED.MVC2.Domain.Model.Entities;
 using _20GRPED.MVC2.Domain.Model.Interfaces.Services;
 using System.Text.Json;
 using _20GRPED.MVC2.Domain.Model.Options;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
@@ -18,60 +21,149 @@ namespace _20GRPED.MVC2.Mvc.HttpServices
         private readonly HttpClient _httpClient;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IOptionsMonitor<BibliotecaHttpOptions> _bibliotecaHttpOptions;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public LivroHttpService(
             IHttpClientFactory httpClientFactory,
-            IOptionsMonitor<BibliotecaHttpOptions> bibliotecaHttpOptions)
+            IOptionsMonitor<BibliotecaHttpOptions> bibliotecaHttpOptions,
+            IHttpContextAccessor httpContextAccessor,
+            SignInManager<IdentityUser> signInManager)
         {
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
             _bibliotecaHttpOptions = bibliotecaHttpOptions ?? throw new ArgumentNullException(nameof(bibliotecaHttpOptions));
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            _signInManager = signInManager;
+            ;
 
             _httpClient = httpClientFactory.CreateClient(bibliotecaHttpOptions.CurrentValue.Name);
             _httpClient.Timeout = TimeSpan.FromMinutes(_bibliotecaHttpOptions.CurrentValue.Timeout);
         }
 
+        private async Task<bool> AddAuthJwtToRequest()
+        {
+            var jwtCookieExists = _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue("bibliotecaToken", out var jwtFromCookie);
+            if (!jwtCookieExists)
+            {
+                await _signInManager.SignOutAsync();
+                return false;
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtFromCookie);
+            return true;
+        }
+
         public async Task<IEnumerable<LivroEntity>> GetAllAsync()
         {
-            var result = await _httpClient.GetStringAsync(_bibliotecaHttpOptions.CurrentValue.LivroPath);
-            return JsonConvert.DeserializeObject<List<LivroEntity>>(result);
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return null;
+            }
+            var httpResponseMessage = await _httpClient.GetAsync(_bibliotecaHttpOptions.CurrentValue.LivroPath);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<List<LivroEntity>>(await httpResponseMessage.Content.ReadAsStringAsync());
         }
 
         public async Task<LivroEntity> GetByIdAsync(int id)
         {
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return null;
+            }
             var pathWithId = $"{_bibliotecaHttpOptions.CurrentValue.LivroPath}/{id}";
-            var result = await _httpClient.GetStringAsync(pathWithId);
-            return JsonConvert.DeserializeObject<LivroEntity>(result);
+            var httpResponseMessage = await _httpClient.GetAsync(pathWithId);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<LivroEntity>(await httpResponseMessage.Content.ReadAsStringAsync());
         }
 
         public async Task InsertAsync(LivroEntity insertedEntity)
         {
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return;
+            }
             var uriPath = $"{_bibliotecaHttpOptions.CurrentValue.LivroPath}";
 
             var httpContent = new StringContent(JsonConvert.SerializeObject(insertedEntity), Encoding.UTF8, "application/json");
 
-            await _httpClient.PostAsync(uriPath, httpContent);
+            var httpResponseMessage = await _httpClient.PostAsync(uriPath, httpContent);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+            }
         }
 
         public async Task UpdateAsync(LivroEntity updatedEntity)
         {
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return;
+            }
             var pathWithId = $"{_bibliotecaHttpOptions.CurrentValue.LivroPath}/{updatedEntity.Id}";
 
             var httpContent = new StringContent(JsonConvert.SerializeObject(updatedEntity), Encoding.UTF8, "application/json");
 
-            await _httpClient.PutAsync(pathWithId, httpContent);
+            var httpResponseMessage = await _httpClient.PutAsync(pathWithId, httpContent);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+            }
         }
 
         public async Task DeleteAsync(int id)
         {
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return;
+            }
+            await AddAuthJwtToRequest();
             var pathWithId = $"{_bibliotecaHttpOptions.CurrentValue.LivroPath}/{id}";
-            await _httpClient.DeleteAsync(pathWithId);
+            var httpResponseMessage = await _httpClient.DeleteAsync(pathWithId);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+            }
         }
 
         public async Task<bool> CheckIsbnAsync(string isbn, int id)
         {
+            var jwtSuccess = await AddAuthJwtToRequest();
+            if (!jwtSuccess)
+            {
+                return false; //rever, pode ser que tenha que inverter ou levantar uma exceção
+            }
+
+            await AddAuthJwtToRequest();
             var pathWithId = $"{_bibliotecaHttpOptions.CurrentValue.LivroPath}/CheckIsbn/{isbn}/{id}";
-            var result = await _httpClient.GetStringAsync(pathWithId);
-            return bool.Parse(result);
+            var httpResponseMessage = await _httpClient.GetAsync(pathWithId);
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                await _signInManager.SignOutAsync();
+                return false; //rever, pode ser que tenha que inverter ou levantar uma exceção
+            }
+
+            return bool.Parse(await httpResponseMessage.Content.ReadAsStringAsync());
         }
     }
 }
